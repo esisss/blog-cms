d2dd719562e04b7ce372f235cc95010485c4e03c
Esis <jesisp17@gmail.com>
2026-02-28 17:13:36 -0300

    ui(nav): tweaked navbar layout and added back button on auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,6 @@
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function AuthLayout({
   children,
 }: {
@@ -5,6 +8,10 @@ export default function AuthLayout({
 }) {
   return (
     <div className="min-h-screen flex items-center justify-center bg-base-200 p-4">
+      <Link className="font-bold fixed top-3 left-3 hover:underline" href="/">
+        <ChevronLeft className="inline" />
+        Back
+      </Link>
       <div className="card bg-base-100 w-full max-w-sm sm:max-w-md shadow-xl">
         <div className="card-body">{children}</div>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Book, Menu } from "lucide-react";
 import { headers } from "next/headers";
 import Link from "next/link";
 import { signOut } from "@/app/(auth)/actions/authServerActions";
@@ -9,38 +10,74 @@ export async function Navbar() {
   });
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm md:px-10 px-3">
       <div className="flex-1">
-        <Link href="/" className="btn btn-ghost text-xl">
-          Blog CMS
+        <Link href="/" className="btn btn-ghost text-xl text-nowrap">
+          <Book className="w-6 h-6 " />
+          <span className="hidden md:inline">Blogtise</span>
         </Link>
       </div>
 
-      <div className="flex-none gap-2">
-        <div className="form-control">
+      <div className="flex flex-row justify-between w-full  gap-2">
+        <div className="form-control w-full">
           <input
             type="text"
             placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
+            className="input input-bordered sm-w-full mx-auto md:w-1/2"
           />
         </div>
 
-        {session ? (
-          <form action={signOut}>
-            <button type="submit" className="btn btn-ghost">
-              Sign out
-            </button>
-          </form>
-        ) : (
-          <>
-            <Link href="/signin" className="btn btn-ghost">
-              Sign in
-            </Link>
-            <Link href="/signup" className="btn btn-primary">
-              Sign up
-            </Link>
-          </>
-        )}
+        <div className="hidden md:flex gap-2">
+          {session ? (
+            <form action={signOut}>
+              <button type="submit" className="btn btn-ghost">
+                Sign out
+              </button>
+            </form>
+          ) : (
+            <>
+              <Link href="/signin" className="btn btn-ghost">
+                Sign in
+              </Link>
+              <Link href="/signup" className="btn btn-primary">
+                Sign up
+              </Link>
+            </>
+          )}
+        </div>
+
+        <div className="dropdown dropdown-end md:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-square">
+            <Menu className="w-6 h-6" />
+          </div>
+          <ul className="dropdown-content menu bg-base-100 rounded-box z-50 mt-5 w-52 p-2 shadow">
+            {session ? (
+              <li>
+                <form action={signOut}>
+                  <button
+                    type="submit"
+                    className="btn btn-ghost w-full justify-start"
+                  >
+                    Sign out
+                  </button>
+                </form>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link href="/signin" className="btn mb-2">
+                    Sign in
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/signup" className="btn btn-primary mb-1 w-full ">
+                    Sign up
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
